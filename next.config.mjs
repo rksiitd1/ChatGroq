9280d29b01dc01ce86d3a446cef55e7829346354
RK Sharma <rksiitd123@gmail.com>
2024-08-17 23:26:18 +0530

    Update configuration for static export and GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  };
-  
-  // Only add basePath and assetPrefix for production builds
-  if (process.env.NODE_ENV === 'production') {
-    nextConfig.basePath = '/ChatGroq';
-    nextConfig.assetPrefix = '/ChatGroq/';
-  }
-  
-  export default nextConfig;
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+};
+
+// Only add basePath and assetPrefix for production builds
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.basePath = '/ChatGroq';
+  nextConfig.assetPrefix = '/ChatGroq/';
+}
+
+export default nextConfig;
