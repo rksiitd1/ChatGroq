0879430859e45e19bf4ed97c7848339d88589a58
RK Sharma <rksiitd123@gmail.com>
2024-08-18 07:43:19 +0530

    fixed relative import path "groq-sdk"

--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -1,10 +1,10 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-import { Groq } from 'groq-sdk'
+import { Groq } from "https://esm.sh/groq-sdk"
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': '*',
-  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
 }
 
 serve(async (req) => {
