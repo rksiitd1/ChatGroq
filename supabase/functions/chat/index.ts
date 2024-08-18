62d83b48b7a120d21e9014358fc914eb30f0092d
RK Sharma <rksiitd123@gmail.com>
2024-08-18 07:15:44 +0530

    resolve some error

--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -2,9 +2,9 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { Groq } from 'groq-sdk'
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://rksiitd1.github.io',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
 }
 
 serve(async (req) => {
